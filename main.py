4b48cc30346d539c552403d3dbf8471f0310f800
Andrew K <rembozebest@gmail.com>
2020-05-24 22:57:19 +0300

    main.py update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tg_test.conversationbot2 as test_conversation_bot
+import tg_manager_bot.bot as manager_bot
 
 if __name__ == '__main__':
-    print('Run test bot')
-    test_conversation_bot.main()
+    manager_bot.main()
